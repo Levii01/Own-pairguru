544d7586bab420eadc9c141c39cc2b01f61fa307
Miłosz Sędziak <m.sedziak01@gmail.com>
2018-10-19 08:46:25 +0200

    Refactro comment_controller and create comment service

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,35 +1,19 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(comment_params)
-    if current_user.has_role?(:spamer) && comment_exists?
-      return redirect_to_movie("Your comment already exists")
-    end
-
-    if @comment.save
-      redirect_to_movie("Comment was successfully created.")
-    else
-      flash[:comment_error] = @comment.errors.full_messages
-      redirect_to_movie("Comment wasn't created")
-    end
+    response = CommentServices::CreateComment.call(params, current_user)
+    flash[:comment_error] = response[:flash] if response[:flash]
+    redirect_to_movie(response[:movie_id], response[:message])
   end
 
   def destroy
     comment = Comment.find(params[:id])
     comment.destroy
-    redirect_to movie_path(comment.movie_id), notice: "Comment was successfully destroyed."
+    redirect_to_movie(comment.movie_id, "Comment was successfully destroyed.")
   end
 
   private
 
-  def redirect_to_movie(notice)
-    redirect_to movie_path(comment_params[:movie_id]), notice: notice
-  end
-
-  def comment_exists?
-    Comment.exists?(movie_id: comment_params[:movie_id], user_id: current_user.id)
-  end
-
-  def comment_params
-    @comment_params ||= params.require(:comment).permit(:body, :user_id, :movie_id)
+  def redirect_to_movie(movie_id, message)
+    redirect_to movie_path(movie_id), notice: message
   end
 end
